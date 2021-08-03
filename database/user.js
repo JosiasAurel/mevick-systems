f8635f3008ccfb9c98d8de539681c0f20c6cf6e6
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-03 13:37:33 +0100

    add function to create user

---
+++ database/user.js
@@ -0,0 +1,37 @@
+
+// this file will contain helper functions to interact with mongo database
+
+// this file will handle the operations for users specifically
+
+const { User } = require("../models/index");
+
+function createUser(name, email, password, role) {
+
+    let isSuccess = false;
+
+    let userRegistrationDate = new Date().toString();
+
+    let newUser = User.create({
+        name,
+        email,
+        password,
+        role,
+        registrationDate: userRegistrationDate
+    }, (error, createdUser) => {
+        if (error) {
+            return;
+        }
+
+        isSuccess = true;
+    });
+
+    newUser.save(); // save newly created user to database
+
+    if (isSuccess) {
+        return {
+            status: "success"
+        }
+    } else {
+        return { status: false }
+    }
+}
