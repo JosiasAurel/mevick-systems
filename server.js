3dc600519fd190a0a2309663ddd7ae5e81175012
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-03 10:31:33 +0100

    add sample home page and dashboard page route

--- server.js
+++ server.js
@@ -13,9 +13,25 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.get("/{page}", (req, res) => {
+app.get("/:page", (req, res) => {
     res.render(req.params.page);
 });
 
+app.get("/home/{page}", (req, res) => {
+    let sampleArticle = {
+        title: "It works",
+        shortDescription: "Does it really work ?",
+        readTime: "2",
+        publishDate: "Aug 03 2021"
+    };
+
+    res.render(__dirname + `/dashboard/${req.params.page}`, {
+        article: sampleArticle
+    });
+});
+
+app.get("/dashboard/{page}", (req, res) => {
+    res.render(__dirname + `/dashboard/${req.params.page}`);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
