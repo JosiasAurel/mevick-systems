4066976b3104a523fc452d88a0f98674726783bc
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-03 10:57:37 +0100

    broken server

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const ejs = require("ejs");
+const { readFileSync } = require("fs");
 
 const app = express();
 
 const port = 3000;
 
+/* // creating a custom file loader for ejs
+const componentsLoader = (path) => {
+    return readFileSync(`${__dirname}/views/components/${path}`);
+};
+
+// set ejs loader
+ejs.fileLoader = componentsLoader;
+ */
+
 // set templating engine
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use("/components", express.static(__dirname + "views/components"));
 
 app.get('/', (req, res) => {
     res.render("index");
@@ -18,6 +30,7 @@ app.get("/:page", (req, res) => {
 });
 
 app.get("/home/{page}", (req, res) => {
+
     let sampleArticle = {
         title: "It works",
         shortDescription: "Does it really work ?",
@@ -25,13 +38,13 @@ app.get("/home/{page}", (req, res) => {
         publishDate: "Aug 03 2021"
     };
 
-    res.render(__dirname + `/dashboard/${req.params.page}`, {
-        article: sampleArticle
+    res.render(`/dashboard/${req.params.page}`, {
+        article: article
     });
 });
 
 app.get("/dashboard/{page}", (req, res) => {
-    res.render(__dirname + `/dashboard/${req.params.page}`);
+    res.render(`/dashboard/${req.params.page}`);
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
