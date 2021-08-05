87773fcdd499032de75ed764dac9589cbc49bd1b
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-05 12:56:42 +0100

    basic server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,4 +2,8 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World from basic server");
+});
+
 app.listen(4000, () => console.log("Working on port 4000"));
