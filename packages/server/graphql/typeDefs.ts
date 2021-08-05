4e567ceabe795610d4e48fc8ef92f6a675fcebaa
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-05 13:10:38 +0100

    create basic type definitions

---
+++ packages/server/graphql/typeDefs.ts
@@ -0,0 +1,30 @@
+
+import { gql } from "apollo-server-express";
+
+const typeDefinitions: any = gql`
+    type User {
+        name: String!
+        email: String!
+        id: ID!
+    }
+
+    type Article {
+        title: String!
+        content: String!
+        readTime: Int!
+        id: ID!
+    }
+
+    type Query {
+        getUsers: [User]
+        getUser(id: ID): User
+        getArticles: [Article]
+        getArticle(id: ID): Article
+    }
+
+    type Mutation {
+        createArticle(title: String!, content: String!, readTime: Int!): Article
+        deleteArticle(id: ID!): String
+        updateArticle(id: ID!, title: String, content: String, readTime: Int): Article
+    }
+`;
