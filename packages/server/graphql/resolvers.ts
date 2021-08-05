017603de9807757034611ec71ac439291321fd07
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-05 15:02:06 +0100

    add basic resolvers

--- packages/server/graphql/resolvers.ts
+++ packages/server/graphql/resolvers.ts
@@ -1,7 +1,34 @@
 
+import { User } from "../database/user";
+
 const resolvers = {
     Query: {
-        
+
+        getUsers: async () => {
+
+            let isError: any = false;
+
+            let fetchedUsers: Array<any> = [];
+
+            let unval: any; // dirty trick that works
+
+            let users__ = await User.find((error: any, users_) => {
+                if (error) {
+                    isError = true;
+                }
+
+                fetchedUsers = users_;
+                return fetchedUsers;
+            });
+
+            unval = await users__;
+
+            if (isError) {
+                return {status: "Error"}
+            } else {
+                return fetchedUsers;
+            }
+        }
     }
 }
 
