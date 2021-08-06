090cb52c318a508bb44da24287c38ce80e209485
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-06 10:03:54 +0100

    create resolver for getting user

--- packages/server/graphql/resolvers.ts
+++ packages/server/graphql/resolvers.ts
@@ -28,6 +28,33 @@ const resolvers = {
             } else {
                 return fetchedUsers;
             }
+        },
+
+        getUser: async (parent: any, args: any) => {
+
+            // extract the user ID from GraphQL params
+            const userId: string = args.id;
+
+            let isError: boolean = false; // error checker
+            let fetchedUser: any; // the fetched user
+
+            let unval: any; // dirty but working trick
+
+            let user__ = await User.findById(userId, (error: any, user_: any) => {
+                if (error) {
+                    isError = true;
+                }
+
+                fetchedUser = user_;
+            });
+
+            unval = await user__;
+
+            if (!isError) {
+                return fetchedUser;
+            } else {
+                return {status: "Failed"}
+            }
         }
     }
 }
