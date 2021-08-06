d36c4d086cc84188cc5b6a2843af1763b7bb59e0
zenith-noble01 <webdev686@gmail.com>
2021-08-06 10:50:48 +0100

    commit-my changes

---
+++ packages/client/pages/_app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/globals.css'
+
+
+function MyApp({ Components, pageProps}) {
+    return (
+        <Components{...pageProps} />
+    )
+}
+
+export default MyApp
