ee900b371521847278a173fc22bdbc3bd99a6b80
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-06 09:18:32 +0100

    add role field to user

--- packages/server/database/user.ts
+++ packages/server/database/user.ts
@@ -4,12 +4,14 @@ interface IUser extends Document {
     name: string
     email: string
     password: string
+    role: string
 }
 
 const UserSchema = new Schema<IUser>({
     name: String,
     email: String,
-    password: String
+    password: String,
+    role: String
 });
 
 const User = model("User", UserSchema);
