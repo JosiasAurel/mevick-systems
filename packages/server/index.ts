8b352c1129dda0708eabe7c28f7d87d38db0890b
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-06 09:20:32 +0100

    add endpoint for user registration

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,8 +2,34 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
+// import models
+import { User } from "./database";
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World from basic server");
+});
+
+app.post("/signup", (req: Request, res: Response) => {
+
+    // get the new user credentials
+    const { name, email, password, role } = req.body;
+
+    let isError: boolean = false;
+
+    let newUser: any = User.create({name, email, password, role}, (error: any, user: any) => {
+        if (error) {
+            isError = true;
+            return;
+        }
+
+        return;
+    });
+
+    if (!isError) {
+        return {status: "Success"}
+    } else {
+        return {status: "Failed"}
+    }
 });
 
 app.listen(4000, () => console.log("Working on port 4000"));
