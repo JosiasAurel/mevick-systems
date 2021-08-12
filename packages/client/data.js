8a754ae704d1307c2dd987fc997ca49554c28ae2
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-12 09:53:16 +0100

    state -> style

--- packages/client/data.js
+++ packages/client/data.js
@@ -18,14 +18,17 @@ const offersInfo = [
 const nursery = [
     {
         title: "Nursery One",
+        style: "nurseryOne",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
     {
         title: "Nursery Two",
+        style: "nurseryTwo",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
     {
         title: "Nursery Three",
+        style: "nurseryThree",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
 ];
@@ -33,14 +36,17 @@ const nursery = [
 const primary = [
     {
         title: "Primary One",
+        style: "primaryOne",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
     {
         title: "Primary Three",
+        style: "primaryTwo",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
     {
         title: "Primary Six",
+        style: "primaryThree",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
 ]
@@ -48,14 +54,17 @@ const primary = [
 const secondary = [
     {
         title: "Secondary/Commercial",
+        style: "secondaryOne",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
     {
         title: "Secondary-second-cycle",
+        style: "secondaryTwo",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
     {
         title: "Commercial-second-Cycle",
+        style: "secondaryThree",
         content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
     },
 ];
