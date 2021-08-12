92d1576b52f0a95bbb4ced3d9513168b777f844c
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-12 09:45:14 +0100

    dummy data

---
+++ packages/client/data.js
@@ -0,0 +1,63 @@
+
+// dummy data
+const offersInfo = [
+    {
+        title: "Nursery",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eos impedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Primary",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eos impedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Secondary/Commercial",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eos impedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+];
+
+const nursery = [
+    {
+        title: "Nursery One",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Nursery Two",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Nursery Three",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+];
+
+const primary = [
+    {
+        title: "Primary One",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Primary Three",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Primary Six",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+]
+
+const secondary = [
+    {
+        title: "Secondary/Commercial",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Secondary-second-cycle",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+    {
+        title: "Commercial-second-Cycle",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut sit eosimpedit est architecto temporibus eligendi, repellendus cum quod!"
+    },
+];
+
+export { offersInfo, nursery, primary, secondary };
