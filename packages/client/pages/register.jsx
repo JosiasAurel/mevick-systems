e4427a13dffbd8ef4d45a9530271bfbe3538eb3a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-13 10:14:05 +0100

    create register page

--- packages/client/pages/register.jsx
+++ packages/client/pages/register.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Link from 'next/link'
 
+// import styles
+import styles from "../styles/register.module.css";
+
 const Register = () => {
     return (
-        <div>
-            <h2>Do work here</h2>
-             {/* only students register because Teachers will be hard coded into the database */}
+        <div className={styles.registerPage}>
+            <header className={styles.header}>
+                <h1 className={styles.logo}>Mevick</h1>
+            </header>
+
+            <div className={styles.form__elements}>
+                <div>
+                <h2>Register</h2>
+                <div className={styles.formContainer}>
+                <form>
+                    <input type="text" placeholder="User name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button>
+                        Register
+                    </button>
+                </form>
+            </div>
+            </div>
+            </div>
         </div>
     )
 }
