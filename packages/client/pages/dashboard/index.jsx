270aacbb628fe5c589b02823e9160dbb9c55443e
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-15 22:52:06 +0100

    create dadhboard index

--- packages/client/pages/dashboard/index.jsx
+++ packages/client/pages/dashboard/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import styles from "../../styles/dashboard.module.css";
+
 const DashboardPage = () => {
     return (
-        <div>
-            
+        <div className={styles.dashboardIndexPage}>
+            <header className={styles.header}>
+                <h2 className={styles.logo}>Mevick Systems_</h2>
+            </header>
+
+            <article>
+                <div>
+                    <h2>Title</h2>
+                    <span>
+                        <p> 5 min(s) read </p>
+                        <p> Sun 15 Aug 2021 </p>
+                    </span>
+                </div>
+            </article>
         </div>
     )
 }
