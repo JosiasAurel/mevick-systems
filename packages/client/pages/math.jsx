3731ba57c699c7e24babcbadffecdaa492099ba1
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-16 02:35:01 +0100

    add page for math tools

---
+++ packages/client/pages/math.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+// math tools
+import QuadraticSolver from "../tools/quadratic";
+
+const MathToolsPage = () => {
+    return (
+        <div>
+            <div>
+                <h2>Quadratic Solver</h2>
+                <QuadraticSolver />
+            </div>
+        </div>
+    )
+}
+
+export default MathToolsPage;
