5936d5c8515660cbf7df66bfbe28c97ed9ad8259
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-16 10:04:04 +0100

    add favicon

---
+++ packages/client/pages/_document.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+            <link rel="shortcut-icon" href="/favicon.PNG" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
